30c143a0dc16013698b013909c1eaf21c31c2e2c
libresoul <contact@damindu.me>
2026-06-05 08:03:34 +0530

    feat: add custom password provider and write additional user fields to teams table
    - add custom user, teams table schema
    - setup custom password provider for passing additional fields
    - write teamMember into to teams table upon signUp

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,6 +1,42 @@
-import { Password } from "@convex-dev/auth/providers/Password";
+import CustomPassword from "./CustomProfile";
 import { convexAuth } from "@convex-dev/auth/server";
+import { internal } from './_generated/api'
+import { MutationCtx } from "./_generated/server";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [Password],
-})
+  providers: [CustomPassword],
+  callbacks: {
+    async createOrUpdateUser(ctx: MutationCtx, args) {
+      if (args.existingUserId) {
+        return args.existingUserId;
+      }
+
+      const profile = args.profile as {
+        email: string;
+        role?: 'admin' | 'user';
+        phone?: string;
+        teamName?: string;
+        teamLeaderName?: string;
+        teamMembers?: { name: string; studentId: string }[];
+      };
+
+      const userId = await ctx.db.insert("users", {
+        email: profile.email,
+        role: 'user',
+        phone: profile.phone!,
+      });
+
+      if (profile.teamName && profile.teamLeaderName && profile.teamMembers) {
+        await ctx.db.insert("teams", {
+          userId,
+          teamName: profile.teamName,
+          teamLeaderName: profile.teamLeaderName,
+          teamMembers: profile.teamMembers,
+        });
+
+      }
+
+      return userId;
+    },
+  },
+});
